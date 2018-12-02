8da09ef4d297cbb1dfbb758bda0a642b9dac8cdc
Pavlovi <milan.pavel1@gmail.com>
2018-12-02 18:39:28 +0100

    introducing setupTests.js to pre-configure tests in the project

---
+++ src/setupTests.js
@@ -0,0 +1,5 @@
+// this file is automatically found by Jest based on create-react-app configuration (jest configuration)
+import Enzyme from 'enzyme';
+import EnzymeAdapter from 'enzyme-adapter-react-16';
+
+Enzyme.configure({ adapter: new EnzymeAdapter() });
