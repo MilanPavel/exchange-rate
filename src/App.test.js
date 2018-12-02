5c256f41114de1393c509878701bf813865477df
Pavlovi <milan.pavel1@gmail.com>
2018-12-02 18:38:12 +0100

    introducing setupTests.js to pre-configure tests in the project

--- src/App.test.js
+++ src/App.test.js
@@ -1,19 +1,44 @@
 import React from 'react';
-import Enzyme, { shallow } from 'enzyme';
-import EnzymeAdapter from 'enzyme-adapter-react-16';
+import { shallow } from 'enzyme';
 
 import App from './App';
 
-Enzyme.configure({ adapter: new EnzymeAdapter() });
+/*
+ *  Factory function to create a ShallowWrapper for the App component.
+ *  @function setup
+ *  @param {object} props - Component props specific for his setup.
+ *  @param {object} state - Initial state for setup.
+ *  @returns {ShallowWrapper}
+ */
+const setup = (props = {}, state = null) => {
+  const wrapper = shallow(<App {...props} />);
+  if (state) wrapper.setState(state);
+  return wrapper;
+};
+
+/*
+ * Return ShallowWrapper containing node(s) with the given data-test value
+ * @param {ShallowWrapper} wrapper - Enzyme shallow wrapper to search within.
+ * @param {string} val - Value of data-test attribute for search.
+ */
+const findByTestAttr = (wrapper, val) => {
+  return wrapper.find(`[data-test="${val}"]`);
+};
 
 test('renders without errors', () => {
-  const wrapper = shallow(<App />);
-  const appComponent = wrapper.find("[data-test='component-app']");
+  const wrapper = setup();
+  const appComponent = findByTestAttr(wrapper, 'component-app');
   expect(appComponent.length).toBe(1);
 });
 
 test('renders header component', () => {
-  const wrapper = shallow(<App />);
-  const appComponent = wrapper.find("[data-test='component-header']");
+  const wrapper = setup();
+  const appComponent = findByTestAttr(wrapper, 'component-header');
   expect(appComponent.length).toBe(1);
+});
+
+test('InputCZK starts at 1', () => {
+  const wrapper = setup();
+  const initialInputState = wrapper.state('inputCZK');
+  expect(initialInputState).toBe('1');
 });
